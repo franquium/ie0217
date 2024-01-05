ce34298bc37277ecc091c8324bdb01a480b2234e
franquium <jose.franchichongkan@ucr.ac.cr>
2024-01-04 21:06:34 -0600

    Codigo del switch-case del Video

---
+++ Previos/Previo_1/Sesion_2/switchcaseVideo.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+int main(){
+    
+    int var1 = 52;
+    switch (var1){
+        case 1:
+            std::cout  << "Case 1!\n" ;
+            break;
+        case 2:
+    
+        case 3:
+            std::cout  << "Case Final!\n" ;
+            break;
+       
+        default:
+            std::cout << "Case Default!\n";
+            break;
+    }
+
+
+
+    return 0;
+}
