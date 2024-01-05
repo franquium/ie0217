727bf939883eb6d2e38b5f7ac6c55d03ce53ff98
franquium <jose.franchichongkan@ucr.ac.cr>
2024-01-04 20:52:30 -0600

    Codigo de condicionales

---
+++ Previos/Previo_1/Sesion_2/condicionales.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+
+int main(){
+    
+    int number = 5;
+    // Condicional IF
+
+    // Condicion es verdadera
+    if (number > 0){
+        // Se ejecuta este codigo
+    }
+
+
+    // Condicion es falsa
+    if (number < 0){
+        // Se ejecuta este codigo
+    }
+
+
+    // Condicional IF, ELSE
+
+    // Condicion es verdadera
+    if (number > 0){
+        // Se ejecuta este codigo
+    }
+    else {
+        // Se ejecuta este codigo
+    }
+
+    // Condicion es falsa
+    if (number < 0){
+        // Se ejecuta este codigo
+    }
+    else {
+        // Se ejecuta este codigo
+    }
+
+    // Condicional IF, ELSE-IF, ELSE
+
+    // Condicion es verdadera 1era
+    int number = 2;
+    if (number > 0){
+        // Se ejecuta este codigo
+    }
+    else if (number == 0){
+        // Se ejecuta este codigo
+    }
+    else {
+        // Se ejecuta este codigo
+    }
+
+    // Condicion es verdadera 2da
+    int number = 0;
+    if (number > 0){
+        // Se ejecuta este codigo
+    }
+    else if (number == 0){
+        // Se ejecuta este codigo
+    }
+    else {
+        // Se ejecuta este codigo
+    }
+
+    // Condiciones son falsas
+    int number = -2;
+    if (number > 0){
+        // Se ejecuta este codigo
+    }
+    else if (number == 0){
+        // Se ejecuta este codigo
+    }
+    else {
+        // Se ejecuta este codigo
+    }
+
+    return 0;
+}
