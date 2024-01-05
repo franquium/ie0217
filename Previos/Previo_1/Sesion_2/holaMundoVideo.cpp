a980d6473bb748d95f3d0b4b28da3c5a6dbd08bc
franquium <jose.franchichongkan@ucr.ac.cr>
2024-01-04 20:43:31 -0600

    Codigo del video se usa operador ternario

---
+++ Previos/Previo_1/Sesion_2/holaMundoVideo.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main(){
+    std:: cout << "Hola mundo, terrestres del IE0217!\n";
+
+    int var1 = 0;
+    int var2;
+
+    var2 = (var1 == 3) ? 15 : 27;
+
+    std::cout << "var2: \n" << var2 << "";
+    
+
+    return 0;
+}
