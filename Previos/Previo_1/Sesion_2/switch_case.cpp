7ced9d625b70207d6d2aae787b660103b849d06b
franquium <jose.franchichongkan@ucr.ac.cr>
2024-01-04 20:57:22 -0600

    Codigo del switch-case

---
+++ Previos/Previo_1/Sesion_2/switch_case.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+
+int main(){
+    char oper; 
+    float num1, num2;
+    cout << "Digite un operador (+, -, *, /):";
+    cin >> oper;
+    cout << "Ingrese 2 numeros: " << endl;
+    cin >> num1 >> num2;
+
+    switch (oper){
+        case '+':
+            cout << num1 << " + " << num2 << " = " << num1 + num2;
+            break;
+        case '-':
+            cout << num1 << " - " << num2 << " = " << num1 - num2;
+            break;
+        case '*':
+            cout << num1 << " * " << num2 << " = " << num1 * num2;
+            break;
+        case '/':
+            cout << num1 << " / " << num2 << " = " << num1 / num2;    
+            break;
+        default:
+            // El Operador no concuerda con ninguno de (+, -, *, /)
+            cout << "Error! El Operador no es correcto"
+            break;
+    }
+
+    return 0;
+}
