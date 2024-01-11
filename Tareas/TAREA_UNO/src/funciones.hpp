5e31dcdd7b2c0be3f87a928afe07c8bde04c2168
franquium <jose.franchichongkan@ucr.ac.cr>
2024-01-11 04:32:51 -0600

    Se agrega documentacion en formato Doxygen para el archivo funciones.hpp
    Signed-off-by: franquium <jose.franchichongkan@ucr.ac.cr>

--- Tareas/TAREA_UNO/src/funciones.hpp
+++ Tareas/TAREA_UNO/src/funciones.hpp
@@ -1,24 +1,36 @@
+/**
+ * @file funciones.hpp
+ * * @author J. Antonio Franchi 
+ * @brief Archivo de headers de funciones
+ * Contiene los headers de las funciones del juego,
+ * asi como la definicion del Struct Juego para los datos requeridos en el juego.
+ * 
+
+ * @copyright Copyleft
+ * 
+ */
 #ifndef FUNCIONES_HPP
 #define FUNCIONES_HPP
 #include <string>
 #include <iostream>
 
-
+// Declarando el struct para los datos del juego
 struct Juego {
     int numMin; // Cota inferior del intervalo
     int numMax; // Cota superior del intervalo
-    int modo;
-    int numAdivinar;
+    int modo;   // Modo de juego
+    int numAdivinar; // Numero aleatorio a adivinar
 
 };
 
+// Colocando los headers de las funciones del programa
 void mostrarMenu();
-void procesarOpcion(Juego& datos);
-void iniciarJuego(Juego& datos);
-void jugarModoEstandar(Juego& datos);
-void jugarModoDificil(Juego& datos);
-int generarNumeroRandom(Juego& datos);
+void procesarOpcion(Juego datos);
+void iniciarJuego(Juego datos);
+void jugarModoEstandar(Juego datos);
+void jugarModoDificil(Juego datos);
+int generarNumeroRandom(Juego datos);
 int getNumeroUsuario();
-int calcularNumIntentos(Juego& datos);
+int calcularNumIntentos(Juego datos);
 
 #endif
