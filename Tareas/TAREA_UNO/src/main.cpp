b355d3f1d8f200df7b58491721b1de76ac0ab948
franquium <jose.franchichongkan@ucr.ac.cr>
2024-01-11 04:21:35 -0600

    Se agrega documentacion en formato Doxygen para el archivo main.cpp
    Signed-off-by: franquium <jose.franchichongkan@ucr.ac.cr>

--- Tareas/TAREA_UNO/src/main.cpp
+++ Tareas/TAREA_UNO/src/main.cpp
@@ -1,3 +1,15 @@
+/**
+ * @file main.cpp
+ * @author J. Antonio Franchi 
+ * @brief Funcion principal del juego
+ * Configura el programa del juego adivinar un numero en un rango definido, 
+ * inicializando los valores predeterminados y ejecuta el loop principal del juego. 
+ * Dentro del loop, muestra el menu y procesa las opciones seleccionadas por el usuario.
+ * 
+ * @copyright Copyleft
+ * 
+ */
+
 #include <iostream>
 #include "funciones.hpp"
 
@@ -6,9 +18,9 @@ int main(){
     Juego datos; // Instancia del struct Juego para mantener el estado del juego
     bool enJuego = true; // Para controlar el loop principal
 
-    // Configuración inicial del juego
-    // a pesar de que se indico asumir que el usuario va a hacer buen uso
-    // se pone como respaldo
+    /**< Configuración inicial del juego
+    * a pesar de que se indico asumir que el usuario va a hacer buen uso
+    * se pone como respaldo */
     datos.numMin = 1; // Valor minimo del intervalo por defecto
     datos.numMax = 20; // Valor maximo del intervalo por defecto
     datos.modo = 1; // Modo de juego por defecto, Modo Estandar
