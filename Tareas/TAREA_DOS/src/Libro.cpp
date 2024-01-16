43c259f99f7d4b53a63d3ef90591c1219d18f4d0
franquium <jose.franchichongkan@ucr.ac.cr>
2024-01-16 02:19:27 -0600

    Agregando archivos premilimares para la clase Libro

---
+++ Tareas/TAREA_DOS/src/Libro.cpp
@@ -0,0 +1,33 @@
+#include "Libro.hpp"
+#include <string>
+using namespace std;
+//#include <iostream>
+//using namespace std;
+
+// Constructor de la clase y la lista de  inicializacion
+    Libro::Libro( string titulo, string grupo, string tipoMaterial, string autor,string editorial,
+                     string genero, string estado, int cantidadHojas, double precio,
+                     string resumenContenido, string materialRelacionado)
+                     : MaterialLectura(titulo, "Lectura", "Libro", autor, editorial, genero, estado, cantidadHojas,precio),
+                       resumenContenido(resumenContenido),
+                       materialRelacionado(materialRelacionado)
+                       {}
+
+    // Metodos o funciones 
+    string Libro::calcularLongitud()  {
+        //Falta codigo
+    }
+
+    void Libro::imprimirInfo() {
+        cout << "Titulo: " << titulo << endl;
+        cout << "Autor: " << autor << endl;
+        cout << "Editorial: " << editorial << endl;
+        cout<< "Genero: " << genero << endl;
+        cout << "Estado: " << estado << endl;
+        cout << "Cantidad de Hojas: " << cantidadHojas << endl;
+        cout << "Precio: " << precio << endl;
+        cout << "Resumen: " << resumenContenido << endl;
+        cout << "Material Relacionado: " << materialRelacionado << endl;
+    }
+    
+
