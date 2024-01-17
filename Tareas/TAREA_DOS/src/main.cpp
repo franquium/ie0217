8bdd5bc4916dfac197782eef34d237c442865494
franquium <jose.franchichongkan@ucr.ac.cr>
2024-01-17 02:41:19 -0600

    Actualizando el main para verificar funcionamiento clases Pelicula y Podcast

--- Tareas/TAREA_DOS/src/main.cpp
+++ Tareas/TAREA_DOS/src/main.cpp
@@ -12,6 +12,8 @@
 
 #include "Libro.hpp"
 #include "Noticia.hpp"
+#include "Pelicula.hpp"
+#include "Podcast.hpp"
 #include <iostream>
 
 /**
@@ -27,11 +29,12 @@ int main(){
      * como informacion veraz sobre libros, noticias y otros. 
      */
 
+
     /* Creando una instancia de la Clase Libro */
     Libro miLibro(
         "El Principito", 
-        "Lectura", 
-        "Libro", 
+        "--Lectura--", 
+        "--Libro--", 
         "Antoine de Saint-Exupery",
         "Editorial C'est la Vie",
         "Ficcion",
@@ -48,12 +51,14 @@ int main(){
     // Calcular y mostrar la longitud del libro
     std::string longitudLibro = miLibro.calcularLongitud();
     std::cout << "Longitud del libro: " << longitudLibro  << std::endl;
+    std::cout << "\n"; // Para imprimir salto linea, por razones esteticas nada mas
+
 
     /* Creando una instancia de la clase Noticia */
     Noticia miNoticia(
         "Violacion de la Covarianza en las Leyes de la Fisica", 
-        "Lectura", 
-        "Noticia", 
+        "--Lectura--", 
+        "--Noticia--", 
         "Isaac Newton",
         "Editorial Patotito",
         "Ciencia",
@@ -67,9 +72,56 @@ int main(){
     // Imprimir info de la noticias
     miNoticia.imprimirInfo();
 
-    // Calcular y mostrar la longitud de la noticias
+    // Calcular y mostrar la longitud de la noticia
     std::string longitudNoticia = miNoticia.calcularLongitud();
     std::cout << "Longitud de la noticia es: " << longitudNoticia  << std::endl;
+    std::cout << "\n"; // Para imprimir salto linea, por razones esteticas nada mas
+
+
+    /* Creando una instancia de la Clase Pelicula */   
+    Pelicula miPeli(
+        "Trainspotting", 
+        "--Audiovisual--", 
+        "--Peli--", 
+        "Danny Boyle",
+        "Comedia-Drama",
+        "Prestado",
+        93,
+        2000.0,
+        "La historia de Renton y sus colegas en el mundo de la heroina la Edimburgo de los 80s...Choose Life!  ",
+        "Peliculas 'edgy' y de humor oscuro"
+    );
+    
+    // Imprimir info de la pelicula
+    miPeli.imprimirInfo();
+
+    // Calcular y mostrar la longitud de la pelicula
+    std::string longitudPeli = miPeli.calcularLongitud();
+    std::cout << "Longitud de la pelicula: " << longitudPeli  << std::endl;
+    std::cout << "\n"; // Para imprimir salto linea, por razones esteticas nada mas
+
+
+    /* Creando una instancia de la Clase Podcast */   
+    Podcast miPodcast(
+        "JRE", 
+        "--Audiovisual--", 
+        "--Podcast--", 
+        "Joe Rogan",
+        "Comedia",
+        "Disponible",
+        120,
+        0.0,
+        "Entrevistas y conversaciones (Talk Show) sobre una basta variedad de temas",
+        "Lex Fridman Podcast and so on"
+    );
+    
+    // Imprimir info del Podcast
+    miPodcast.imprimirInfo();
+
+    // Calcular y mostrar la longitud del Podcast
+    std::string longitudPodcast = miPodcast.calcularLongitud();
+    std::cout << "Longitud del Podcast: " << longitudPodcast  << std::endl;
+    std::cout << "\n"; // Para imprimir salto linea, por razones esteticas nada mas
 
 
 
