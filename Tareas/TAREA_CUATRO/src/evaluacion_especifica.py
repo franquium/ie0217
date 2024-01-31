4e63fc0b49697191a55d62422b2e29cc7d766609
franquium <jose.franchichongkan@ucr.ac.cr>
2024-01-30 21:20:25 -0600

    Actualizando el archivo con la clase Evaluacion Especifica

--- Tareas/TAREA_CUATRO/src/evaluacion_especifica.py
+++ Tareas/TAREA_CUATRO/src/evaluacion_especifica.py
@@ -1,3 +1,41 @@
 
+from alergia import Alergia
+
 class Evaluacion_Especifica:
-    pass
+    """
+    """
+
+    def __init__(self, puntuacion_general):
+        """
+        Constructor de la clase Evaluacion_Especifica.
+        @param puntuacion_general: Puntuacion general de alergias de una persona.
+        """
+        self.puntuacion_general = puntuacion_general
+        self.alergias_personales = self.evaluar_alergias()
+
+    def evaluar_alergias(self):
+        """
+        Evalua las alergias basadas en la puntuacion general.
+        @return: Lista de alergias a las que la persona es alergica.
+        """
+        alergias = []
+        for nombre, valor_alergico in Alergia.alergias_predefinidas:
+            """ @note: Usando las mascara AND bit a bit
+                    Si el resultado de esta operacion AND es diferente de cero, significa 
+                    que la puntuacion general incluye esa alergia especifica, y por lo tanto, 
+                    se agrega a la lista alergias
+            """
+            if (self.puntuacion_general & valor_alergico):
+                alergias.append((nombre, valor_alergico))
+        return alergias
+
+    def mostrar_puntuacion(self):
+        """
+        Imprime la puntuacion general y el desglose.
+        """
+        print(f"Puntuacion General de Alergias: {self.puntuacion_general}")
+        print("Alergias Detectadas:")
+        for nombre, valor_alergico in self.alergias_personales:
+            print(f"{nombre} (Valor: {valor_alergico})")
+
+
--- Tareas/TAREA_CUATRO/src/main.py
+++ Tareas/TAREA_CUATRO/src/main.py
@@ -1,6 +1,7 @@
 # Archivo momentaneo para probar el funcionamiento de las clases
 
 from alergia import Alergia
+from evaluacion_especifica import Evaluacion_Especifica
 
 # Crear una instancia de Alergia
 alergia1 = Alergia("agua", 2048)
@@ -11,3 +12,9 @@ alergia1.mostrar_todas_alergias()
 # Mostrar info de una alergia especifica
 alergia1.mostrar_alergia_especifica("gatos")
 alergia1.mostrar_alergia_especifica("birra")
+print("\n")
+
+# Crear una instancia con una puntuación de 7
+evaluacion = Evaluacion_Especifica(7)
+evaluacion.mostrar_puntuacion()
+print("\n")
