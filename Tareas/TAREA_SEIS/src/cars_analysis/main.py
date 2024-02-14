14e6cd825418c5784912e747967b0274247cf91f
franquium <jose.franchichongkan@ucr.ac.cr>
2024-02-13 22:41:55 -0600

    Actualizando el archivo main regression y el makefile

--- Tareas/TAREA_SEIS/src/cars_analysis/main.py
+++ Tareas/TAREA_SEIS/src/cars_analysis/main.py
@@ -1,16 +1,16 @@
-import regression
-import clustering
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error
-import requests
-import os
+# import requests
+# import os
 from zipfile import ZipFile
 from pathlib import Path
 from kaggle.api.kaggle_api_extended import KaggleApi
+import regression as rl      # Importando los modelos de Regresion Lineal
+import clustering as cl      # Importando los algoritmos de Clustering
 
 # Links dados
 # https://www.kaggle.com/datasets/akshaydattatraykhare/car-details-dataset?resource=download
@@ -18,127 +18,9 @@ from kaggle.api.kaggle_api_extended import KaggleApi
 # https://www.kaggle.com/datasets/akshaydattatraykhare/car-details-dataset/download?datasetVersionNumber=1
 
 
-
-# #########################################
-# Version 1
-# #########################################
-
-# url = "https://www.kaggle.com/datasets/akshaydattatraykhare/car-details-dataset?resource=download"
-
-# # respuesta = requests.get(url)
-
-# # print(respuesta.text)
-
-# # Datos para mi usario de Kaggle
-# username = "antonck"
-# key = "5a685559c7ab3e42c8abf93db8b5c6d2"
-
-# dataset_url = 'https://www.kaggle.com/datasets/akshaydattatraykhare/car-details-dataset?resource=download'
-
-# # Endpoint de la API de Kaggle para descargar datasets
-# api_url = f'https://www.kaggle.com/api/v1/datasets/download/{dataset_url}'
-
-# # Configura los encabezados de la solicitud con tu clave API
-# headers = {'Authorization': f'Bearer {key}'}
-
-# # Realiza la solicitud para descargar el dataset
-# response = requests.get(api_url, headers=headers, stream=True)
-
-# # Verifica si la solicitud fue exitosa
-# if response.status_code == 200:
-#     # Abre un archivo local para guardar el contenido del dataset
-#     with open('dataset.zip', 'wb') as file:
-#         for chunk in response.iter_content(chunk_size=128):
-#             file.write(chunk)
-#     print("Dataset descargado con exito.")
-# else:
-#     print("Error al descargar el dataset:", response.status_code)
-
-# #########################################
-# Version 2
-# #########################################
-
-# car-details-dataset
-# kaggle datasets download -d akshaydattatraykhare/car-details-dataset
-
-# # Datos para mi usario de Kaggle
-# username = "antonck"
-# key = "5a685559c7ab3e42c8abf93db8b5c6d2"
-
-# dataset_name = "car-details-dataset"
-
-# # url_kaggle = f'https://www.kaggle.com/akshaydattatraykhare/{dataset_name}/download'
-# url_kaggle = f'https://www.kaggle.com/datasets/akshaydattatraykhare/car-details-dataset?resource=download&select=CAR+DETAILS+FROM+CAR+DEKHO.csv'
-
-# headers = {"Authorization": f"Bearer {key}"}
-
-# response = requests.get(url_kaggle, headers=headers,  stream=True)
-
-# zip_filename = f'{dataset_name}.zip'
-# with open(zip_filename,"wb") as zip_file:
-#     zip_file.write(response.content)
-
-# with ZipFile(zip_filename, "r") as zip_ref:
-#     zip_ref.extractall("./")
-
-# os.remove(zip_filename)
-
-# #########################################
-# Version3
-# #########################################
-
-# # Debes reemplazar 'your_kaggle_username' y 'your_kaggle_key' con tu nombre de usuario y clave de la API de Kaggle.
-# kaggle_info = {
-#     "UserName": "antonck",
-#     "Key": "5a685559c7ab3e42c8abf93db8b5c6d2"
-# }
-
-# # URL del endpoint de la API para descargar el dataset
-# dataset_url = "https://www.kaggle.com/datasets/akshaydattatraykhare/car-details-dataset?resource=download&select=CAR+DETAILS+FROM+CAR+DEKHO.csv"
-
-# # Path para guardar el archivo .zip del dataset
-# dataset_path = Path("car-details-dataset.zip")
-
-# # Encabezados para la solicitud de la API
-# headers = {
-#     "Authorization": f"Bearer {kaggle_info['Key']}"
-# }
-
-# response = requests.get(dataset_url, headers=headers, stream=True)
-
-# # Verifica que la solicitud fue exitosa (200)
-# if response.status_code == 200:
-#     with open(dataset_path, 'wb') as f:
-#         f.write(response.content)
-#     print(f"Dataset descargado en: {dataset_path}")
-# else:
-#     print(f"Error al descargar el dataset: {response.status_code}")
-
-# #########################################
-# Version 4: Chanchada usando el link Obtenido del HTML
-# #########################################
-
-# # Variable tipo string que contiene el URL
-# url = 'https://www.kaggle.com/datasets/akshaydattatraykhare/car-details-dataset/download?datasetVersionNumber=1'
-
-# # Usando  metodo get para obtener los datos
-# data_web = requests.get(url)
-
-# # # Condicion para crear dir 'data' en caso de que no exista
-# # if not os.path.exists('data'):
-# #     os.mkdir('data')
-
-# # Se abre el archivo (o se crear en caso de no existir)
-# # w: write o escribir
-# # ruta: data/dataset.csv
-# with open('dataset_car-details-dataset.csv', 'wb') as datos:
-#     # Escribir los datos obtenidos del URL en el archivo de la ruta
-#     datos.write(data_web.text)
-
-
-# #########################################
-# Version 5: Usando Kaggle
-# ########################################
+# ###########################################
+# Seccion: Solicitud de Datos usando Kaggle #
+# ###########################################
 
 # Configuracio API key de Kaggle
 api = KaggleApi()
@@ -160,18 +42,21 @@ api.dataset_download_files(dataset_name, path="./", unzip=True)
 # Crear DataFrame de pandas con los datos CSV
 df = pd.read_csv("CAR DETAILS FROM CAR DEKHO.csv")
 print("Datos cargados exitosamente.\n")
+# print(type(df))
 
-# Imprimir pa ver que funke
-print(df.head(5))
-print(df.shape)
+# #####################################################
+# Seccion: Analisis y Limpieza de Datos  de Vehiculos #
+# #####################################################
 
-# Limpieza de Datos  de Vehiculos
+# Analisis exploratorio de los datos usando el metodo describe()
+print(df.describe())
+
 # Bota las columnas donde al menos un elemento falte
 df.dropna()
 
-# Imprimir pa ver que funke
-print(df.head(5))
-print(df.shape)
+# # Imprimir pa ver que funke
+# print(df.head(5))
+# print(df.shape)
 
 # Eliminar datos duplicados del dataset
 df.drop_duplicates()
@@ -180,4 +65,11 @@ df.drop_duplicates()
 print(df.head(5))
 print(df.shape)
 
-print(df.describe())
+
+# ###################################################
+# Seccion: Analisis de Regresion Lineal y No Lineal #
+# ###################################################
+
+# Instanciando el metodo de calcular_RegresionLineal()
+# del archivo regression.py
+rl.calcular_RegresionLineal( df)
--- Tareas/TAREA_SEIS/src/cars_analysis/regression.py
+++ Tareas/TAREA_SEIS/src/cars_analysis/regression.py
@@ -6,141 +6,133 @@ from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error
 from sklearn.metrics import mean_absolute_error
 from sklearn.metrics import r2_score
-# from math import sqrt
 
-def calcular_RegresionLineal():
-    pass
-    
 
-# --- Fin funcion ---- #
+def calcular_RegresionLineal( data):
+
+    # #############
+    # Analisis 1 Relacion del precio con respecto a los annos
+    # #############
+
+    # Extraer datos de la categoria: Variable independiente
+    X_simple = data['year'].values.reshape(-1, 1)
+
+    # Extraer datos de la categoria: Variable dependiente
+    y_simple = data['selling_price'].values
+
+    # Se entrenan el modelo, con 20% datos para la prueba, un seed de 42
+    X_train, X_test, y_train, y_test = train_test_split(X_simple, y_simple, test_size=0.2, random_state=42)
+
+    # Crea y ajusta objeto del tipo de modelo RL
+    model_simple = LinearRegression()
+    model_simple.fit(X_train, y_train)
+
+    # Hacen predicciones del modelo
+    y_pred_simple = model_simple.predict(X_test)
+
+    # Estas metricas ayudan a entender que tan bien se desempenno el modelo
+    # Se obtiene el MSE con simple de la prediccion con respecto a los datos de prueba
+    mse_simple = mean_squared_error(y_test, y_pred_simple)
+    print(f"Error cuadratico medio (MSE) en regresión lineal simple: {mse_simple:.2f}")
+
+    # Error absoluto medio
+    mae_simple = mean_absolute_error(y_test, y_pred_simple)       
+    print(f"Error medio absoluto (MAE) en regresión lineal simple: {mae_simple:.2f}")
+
+    # Calcular el coeficiente de determinacion R^2
+    """ 
+    @note El coeficiente de determinacion R2 es una medida estadistica que 
+        proporciona una indicación de la proporción de la variabilidad en la 
+        variable de respuesta que es explicada por el modelo de regresión
+        R2 = 0: El modelo no explica ninguna variabilidad.
+        R2 = 1: El modelo explica toda la variabilidad
+    """
+    r2 = r2_score(y_test, y_pred_simple)
+    print(f"El Coeficiente de Determinacion R^2: {r2:.2f}")
+
+    # Visualizar los resultados
+    plt.scatter(X_test, y_test, label='Datos de prueba', color='blue')                  # Puntos de prueba
+    # Para agregar las metricas en un Textbox en la grafica
+    props = dict(boxstyle='round', facecolor='wheat', alpha=0.5)
+    metrics = '\n'.join((
+        'MSE: '+ str(f'{mse_simple:.2f}'),
+        'MAE: '+ str(f'{mae_simple:.2f}')
+        ))
+    plt.text(0.05, 0.80, metrics, fontsize=10,
+            horizontalalignment='left', verticalalignment='center_baseline',
+            transform=plt.gca().transAxes, bbox=props)
+    plt.plot(X_test, y_pred_simple, label=f'Regresion Lineal (R^2={r2:.2f})', color='red')     # Linea de regresion
+    plt.title('Variacion Precios de los vehiculos con respecto a los años \
+            \nRegresion Lineal y Coef. de Determinacion R^2')
+    plt.xlabel('X: Ano del vehiculo')
+    plt.ylabel('y: Precio de venta')
+    plt.legend()
+    # Mostrar el grafico
+    plt.show()
+
+
+    # #############
+    # Analisis 2 Relacion del precio con respecto al kilometraje
+    # #############
+
+    # Extraer datos de la categoria: Variable independiente
+    X_simple_2 = data['km_driven'].values.reshape(-1, 1)
+
+    # Extraer datos de la categoria: Variable dependiente
+    y_simple_2 = data['selling_price'].values
+
+    # Se entrenan el modelo, con 20% datos para la prueba, un seed de 42
+    X_train_2, X_test_2, y_train_2, y_test_2 = train_test_split(X_simple_2, y_simple_2, test_size=0.2, random_state=42)
+
+    # Crea y ajusta objeto del tipo de modelo Regresion Lineal Simple
+    model_simple_2 = LinearRegression()
+    model_simple_2.fit(X_train_2, y_train_2)
+
+    # Hacen predicciones del modelo
+    y_pred_simple_2 = model_simple_2.predict(X_test_2)
+
+    # Estas metricas ayudan a entender que tan bien se desempenno el modelo
+    # Se obtiene el MSE con simple de la prediccion con respecto a los datos de prueba
+    mse_simple_2 = mean_squared_error(y_test_2, y_pred_simple_2)
+    print(f"MSE: {mse_simple_2:.2f}")
+
+    # Error absoluto medio
+    mae_simple_2 = mean_absolute_error(y_test_2, y_pred_simple_2)       
+    print(f"Error medio absoluto (MAE) en regresión lineal simple: {mae_simple_2:.2f}")
+
+    # Calcular el coeficiente de determinacion R^2
+    """ 
+    @note El coeficiente de determinacion R2 es una medida estadistica que 
+        proporciona una indicación de la proporción de la variabilidad en la 
+        variable de respuesta que es explicada por el modelo de regresión
+        R2 = 0: El modelo no explica ninguna variabilidad.
+        R2 = 1: El modelo explica toda la variabilidad
+    """
+    r2_2 = r2_score(y_test_2, y_pred_simple_2)
+    print(f"El Coeficiente de Determinacion R^2: {r2_2:.2f}")
+
+    # Visualizar los resultados
+    plt.scatter(X_test_2, y_test_2, label='Datos de prueba', color='red')                  # Puntos de prueba
+    # Para agregar las metricas en un Textbox en la grafica
+    props = dict(boxstyle='round', facecolor='wheat', alpha=0.5)
+    metrics = '\n'.join((
+        'MSE: '+ str(f'{mse_simple_2:.2f}'),
+        'MAE: '+ str(f'{mae_simple_2:.2f}')
+        ))
+    plt.text(0.60, 0.80, metrics, fontsize=10,
+            horizontalalignment='left', verticalalignment='center_baseline',
+            transform=plt.gca().transAxes, bbox=props)
+    plt.plot(X_test_2, y_pred_simple_2, label=f'Regresion Lineal (R^2={r2_2:.2f})', color='green')     # Linea de regresion
+    plt.title('Variacion Precios de los vehiculos con respecto al kilometraje: \
+            \nRegresion Lineal y Coef. de Determinacion R^2')
+    plt.xlabel('X: Kilometraje del vehiculo')
+    plt.ylabel('y: Precio de venta')
+    plt.legend()
+    # Mostrar el grafico
+    plt.show()
+
 
 def calcular_RegresionNoLineal():
     pass
-    
 
 # --- Fin funcion ---- #
-
-# Crear DataFrame de pandas con los datos CSV
-data = pd.read_csv("CAR DETAILS FROM CAR DEKHO.csv")
-print("Datos cargados exitosamente.\n")
-
-# #############
-# Analisis 1 Relacion del precio con respecto a los annos
-# #############
-
-# Extraer datos de la categoria: Variable independiente
-X_simple = data['year'].values.reshape(-1, 1)
-
-# Extraer datos de la categoria: Variable dependiente
-y_simple = data['selling_price'].values
-
-# Se entrenan el modelo, con 20% datos para la prueba, un seed de 42
-X_train, X_test, y_train, y_test = train_test_split(X_simple, y_simple, test_size=0.2, random_state=42)
-
-# Crea y ajusta objeto del tipo de modelo RL
-model_simple = LinearRegression()
-model_simple.fit(X_train, y_train)
-
-# Hacen predicciones del modelo
-y_pred_simple = model_simple.predict(X_test)
-
-# Estas metricas ayudan a entender que tan bien se desempenno el modelo
-# Se obtiene el MSE con simple de la prediccion con respecto a los datos de prueba
-mse_simple = mean_squared_error(y_test, y_pred_simple)
-print(f"Error cuadrático medio (MSE) en regresión lineal simple: {mse_simple:.2f}")
-
-# Error absoluto medio
-mae_simple = mean_absolute_error(y_test, y_pred_simple)       
-print(f"MAE: {mae_simple:.2f}")
-
-# Calcular el coeficiente de determinacion R^2
-""" 
-@note El coeficiente de determinacion R2 es una medida estadistica que 
-      proporciona una indicación de la proporción de la variabilidad en la 
-      variable de respuesta que es explicada por el modelo de regresión
-      R2 = 0: El modelo no explica ninguna variabilidad.
-      R2 = 1: El modelo explica toda la variabilidad
-"""
-r2 = r2_score(y_test, y_pred_simple)
-print(f"Coef R2: {r2:.2f}")
-
-# Visualizar los resultados
-plt.scatter(X_test, y_test, label='Datos de prueba', color='blue')                  # Puntos de prueba
-# Para agregar las metricas en un Textbox en la grafica
-props = dict(boxstyle='round', facecolor='wheat', alpha=0.5)
-metrics = '\n'.join((
-    'MSE: '+ str(f'{mse_simple:.2f}'),
-   'MAE: '+ str(f'{mae_simple:.2f}')
-    ))
-plt.text(0.05, 0.80, metrics, fontsize=10,
-        horizontalalignment='left', verticalalignment='center_baseline',
-        transform=plt.gca().transAxes, bbox=props)
-plt.plot(X_test, y_pred_simple, label=f'Regresion Lineal (R^2={r2:.2f})', color='red')     # Linea de regresion
-plt.title('Variacion Precios de los vehiculos con respecto a los anos: \
-          \nRegresion Lineal y Coef. de Determinacion R^2')
-plt.xlabel('X: Ano del vehiculo')
-plt.ylabel('y: Precio de venta')
-plt.legend()
-# Mostrar el grafico
-plt.show()
-
-
-# #############
-# Analisis 2 Relacion del precio con respecto al kilometraje
-# #############
-
-# Extraer datos de la categoria: Variable independiente
-X_simple_2 = data['km_driven'].values.reshape(-1, 1)
-
-# Extraer datos de la categoria: Variable dependiente
-y_simple_2 = data['selling_price'].values
-
-# Se entrenan el modelo, con 20% datos para la prueba, un seed de 42
-X_train_2, X_test_2, y_train_2, y_test_2 = train_test_split(X_simple_2, y_simple_2, test_size=0.2, random_state=42)
-
-# Crea y ajusta objeto del tipo de modelo Regresion Lineal Simple
-model_simple_2 = LinearRegression()
-model_simple_2.fit(X_train_2, y_train_2)
-
-# Hacen predicciones del modelo
-y_pred_simple_2 = model_simple_2.predict(X_test_2)
-
-# Estas metricas ayudan a entender que tan bien se desempenno el modelo
-# Se obtiene el MSE con simple de la prediccion con respecto a los datos de prueba
-mse_simple_2 = mean_squared_error(y_test_2, y_pred_simple_2)
-print(f"MSE: {mse_simple_2:.2f}")
-
-# Error absoluto medio
-mae_simple_2 = mean_absolute_error(y_test_2, y_pred_simple_2)       
-print(f"MAE: {mae_simple_2:.2f}")
-
-# Calcular el coeficiente de determinacion R^2
-""" 
-@note El coeficiente de determinacion R2 es una medida estadistica que 
-      proporciona una indicación de la proporción de la variabilidad en la 
-      variable de respuesta que es explicada por el modelo de regresión
-      R2 = 0: El modelo no explica ninguna variabilidad.
-      R2 = 1: El modelo explica toda la variabilidad
-"""
-r2_2 = r2_score(y_test_2, y_pred_simple_2)
-print(f"Coef R2: {r2_2:.2f}")
-
-# Visualizar los resultados
-plt.scatter(X_test_2, y_test_2, label='Datos de prueba', color='red')                  # Puntos de prueba
-# Para agregar las metricas en un Textbox en la grafica
-props = dict(boxstyle='round', facecolor='wheat', alpha=0.5)
-metrics = '\n'.join((
-    'MSE: '+ str(f'{mse_simple_2:.2f}'),
-   'MAE: '+ str(f'{mae_simple_2:.2f}')
-    ))
-plt.text(0.60, 0.80, metrics, fontsize=10,
-        horizontalalignment='left', verticalalignment='center_baseline',
-        transform=plt.gca().transAxes, bbox=props)
-plt.plot(X_test_2, y_pred_simple_2, label=f'Regresion Lineal (R^2={r2_2:.2f})', color='green')     # Linea de regresion
-plt.title('Variacion Precios de los vehiculos con respecto al kilometraje: \
-          \nRegresion Lineal y Coef. de Determinacion R^2')
-plt.xlabel('X: Kilometraje del vehiculo')
-plt.ylabel('y: Precio de venta')
-plt.legend()
-# Mostrar el grafico
-plt.show()
